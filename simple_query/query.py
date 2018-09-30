f772442f2b57171ad5d0948d2274dfd9980bad89
Matan Noam Shavit <matannoam@gmail.com>
2018-09-29 20:24:51 -0400

    use matcher class for query

--- simple_query/query.py
+++ simple_query/query.py
@@ -8,14 +8,11 @@ class Query:
 
     def filter(self, field, operator, value):
 
-        def matches_value(target):
-            return operator(target, value)
-
         def get_field(item):
             return getattr(item, field)
 
         def field_matches_value(item):
-            return matches_value(get_field(item))
+            return Matcher(operator, value).matches(get_field(item))
 
         return Query(
             item for item in self.data
